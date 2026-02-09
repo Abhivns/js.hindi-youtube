3c7699342f4f5c913e835cf087e7c62f009c022b
Abhivns <149507405+Abhivns@users.noreply.github.com>
2026-02-09 15:20:27 +0000

    dev changes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountid = 1234567890;
+let accountEmail = "abhi.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+
+accountid = 9876543210; // This will throw an error because accountid is a constant
+console.log(accountid);
+
