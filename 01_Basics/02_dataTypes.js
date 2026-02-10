2fac87a6d184d8a9e1e071b588742b1471beb15f
Abhivns <149507405+Abhivns@users.noreply.github.com>
2026-02-10 14:32:42 +0000

    03 conversion

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";    // treat all JS code as newer version
+
+// alert("Hello World!")    // output Hello World in an alert box
+
+
+// alert(3+3)    // we are using nodejs , not browser, so we will use console.log instead of alert
+
+
+let name = "Abhishek"    // string data type
+let age = 22    // number data type
+let isStudent = true    // boolean data type
+
+
+
+// number => 2 to power 53
+// bigint 
+// string => true/false
+// boolean => true/false
+// null => standalone value empty value, kuch to hai, but khali hai
+// undefined => value is not assigned 
+// symbol => unique identifier
+
+
+
+
+// object => collection of key value pairs
+
+console.log(typeof null);   // object => this is a bug in javascript, but it is not going to be fixed because of backward compatibility
