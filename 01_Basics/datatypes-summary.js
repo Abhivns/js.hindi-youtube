300254c03844d6a89fd3bff30a05250ebe596705
Abhivns <149507405+Abhivns@users.noreply.github.com>
2026-02-12 06:30:09 +0000

    DataTypes-Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,75 @@
+// primitive
+
+// 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+// const
+
+
+
+// Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+
+// const score = 100
+// const scoreValue = 100.3   type - number
+
+// const isLoggedIn = false   type - Boolean
+ // const outsideTemp = null   // type -  object
+ // let userEmail;  // undefinded
+
+ const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId); // false 
+
+ const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+ const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof id);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myFunction);
+
+
+// HomeWork
+
+// const score = 100
+// const scoreValue = 100.3  // type - number
+
+// const isLoggedIn = false //  type - Boolean
+
+ // const outsideTemp = null   // type -  object
+
+ // let userEmail;  // undefinded
+ //  const id = Symbol('123')           // type - symbol
+
+//   const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }                                     // type = object
+
+
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }                                          type - function
+
+
+//  const bigNumber = 3456543576654356754n    type - BigInt
