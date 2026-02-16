6904f40a1feb991b6feff3d6fe991418c171cd0f
Abhivns <149507405+Abhivns@users.noreply.github.com>
2026-02-16 13:09:01 +0000

    date

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,17 @@
+// Dates
+
+
+let myDate = new Date()    // current date and time
+console.log(myDate.toString());
+console.log(myDate.toDateString())
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getTime());   // returns the number of milliseconds since January 1, 1970, 00:00:00 UTC
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());  // month is zero-based, so January is 0 and December is 11
+console.log(myDate.getDate());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getTime()); 
